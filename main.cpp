8c5b5ece1401f88a037a91751f0526d2142113bc
codewithmugisha <bright001it@gmail.com>
2026-08-17 08:50:20 +0200

    bright commited

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main () {
-
+	
+	cout << "bright new features" << endl;
 	cout << "Hello World" << endl;
 	cout << "Hope everyone is at work" << endl;
 	for(int i = 0; i < 5; i++) {
 		cout << "i " << i << endl;
 	}
+
 
 	return 0;
 }
